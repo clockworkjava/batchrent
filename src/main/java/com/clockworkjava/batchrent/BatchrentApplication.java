4f109271e7178ce8ee5375f7172cf375228548e0
Pawel Cwik <cwiku86@gmail.com>
2018-10-26 22:32:20 +0200

    Initial commit

---
+++ src/main/java/com/clockworkjava/batchrent/BatchrentApplication.java
@@ -0,0 +1,12 @@
+package com.clockworkjava.batchrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchrentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/clockworkjava/batchrent/BatchrentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clockworkjava.batchrent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BatchrentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
